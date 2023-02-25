9f74615b764be8bbfaa196c28f85d7d7f63c6913
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2023-02-25 21:28:04 +0530

    Privacy Added

--- src/App.js
+++ src/App.js
@@ -3,41 +3,43 @@ import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/landingPage";
 import Form from "./components/FormPage/Form";
+import Privacy from "./components/Privacy/Privacy";
 
 function App() {
-	const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-	const [currentUser, setCurrentUser] = useState("");
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState("");
 
-	useEffect(() => {
-		const loggedInUser = localStorage.getItem("userinfo");
-		if (loggedInUser) {
-			const foundUser = JSON.parse(loggedInUser);
-			setCurrentUser(foundUser);
-			setIsUserLoggedIn(true);
-		}
-	}, []);
-	return (
-		<div className="App">
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<LandingPage
-							isUserLoggedIn={isUserLoggedIn}
-							setIsUserLoggedIn={setIsUserLoggedIn}
-							currentUser={currentUser}
-							setCurrentUser={setCurrentUser}
-						/>
-					}
-				/>
-				<Route
-					path="/form"
-					element={<Form isUserLoggedIn={isUserLoggedIn} />}
-				/>
-				<Route path="*" component={<div>Error 404</div>} />
-			</Routes>
-		</div>
-	);
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("userinfo");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      setCurrentUser(foundUser);
+      setIsUserLoggedIn(true);
+    }
+  }, []);
+  return (
+    <div className='App'>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <LandingPage
+              isUserLoggedIn={isUserLoggedIn}
+              setIsUserLoggedIn={setIsUserLoggedIn}
+              currentUser={currentUser}
+              setCurrentUser={setCurrentUser}
+            />
+          }
+        />
+        <Route path='/privacy' element={<Privacy />} />
+        <Route
+          path='/form'
+          element={<Form isUserLoggedIn={isUserLoggedIn} />}
+        />
+        <Route path='*' component={<div>Error 404</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
